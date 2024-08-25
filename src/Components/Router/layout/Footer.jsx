f7821b580d661ca6a1361d8a504fd5bb2e70ae12
lucas-noleto <lucas-noleto@outlook.com>
2024-08-25 11:34:16 -0300

    Foot

--- src/Components/Router/layout/Footer.jsx
+++ src/Components/Router/layout/Footer.jsx
@@ -1,6 +1,28 @@
+import styles from './Footer.module.css'
+import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa'
+
+
 function Footer (){
     return(
-        <p>Footer</p>
+        <footer className={styles.footer}>
+            <ul className={styles.social_list}>
+                <li >
+                    <FaFacebook/>
+                </li>
+                <li >
+                    <FaInstagram/>
+                </li>
+                <li >
+                    <FaLinkedin/>
+                </li>
+               
+            </ul>
+            
+            <p className={styles.copy_right}>
+                <span>Costs &copy; 2021 </span> 
+            </p>
+
+        </footer>
     )
 }
 
