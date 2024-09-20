7ceec937cf8c3dc02e6664e98725b72e7b929479
lucas-noleto <lucas_noleto@outlook.com>
2024-09-20 15:36:40 -0300

    Ajustes

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,5 +1,7 @@
 import Message from '../Components/Router/layout/Message'
 import { useLocation } from 'react-router-dom'
+import apiUrl from '../axios/config'
+
 
 function Projects(){
 
@@ -9,12 +11,32 @@ function Projects(){
         message = location.state.message
     }
 
+    function getProject() {
+        
+
+        // Enviando o projeto via POST
+        apiUrl.get("/projects")
+        .then((response) => {
+            console.log("Projetos criados:", response.data);
+            // Navegar após a criação
+            
+        })
+        .catch((error) => {
+            console.error("Erro ao dar get em projetos:", error);
+            // Exibir uma mensagem de erro ou tratar o erro de forma apropriada
+             
+        });
+    
+    }
+
+
     return(
         <div>
             <h1>
                 Meus Projetos
             </h1>
             {message && <Message msg={message} type="success" />}
+            {getProject()}
         </div>
     )
 }
